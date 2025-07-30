570e94d126bf0c9cadfab58ae931b460671dd520
Aryan Lad <aryan52217@gmail.com>
2025-07-31 00:28:12 +0530

    Add implementation of a doubly linked list with dynamic memory allocation

---
+++ doublylinkedlist.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+};
+int main(){
+    int n;
+    cout<<"Enter teh number of nodes:";
+    cin>>n;
+    Node* head=nullptr;
+    Node* tail=nullptr;
+    for(int i=0;i<n;++i){
+        Node* newnode=new Node;
+        cout<<"Enter the data for Node "<<i+1<<": ";
+        cin>>newnode->data;
+        newnode->next=nullptr;
+        newnode->prev=nullptr;
+        if(head==nullptr){
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail->next=newnode;
+            newnode->prev=tail;
+            tail=newnode;
+        }
+    }
+     cout<<"Doubly Linked list :";
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+    // Free allocated memory
+    temp=head;
+    while(temp!=nullptr){
+        Node* next=temp->next;
+        delete temp;
+        temp=next;
+    }
+    return 0;
+}
